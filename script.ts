5bf283fe8f4075ace25a147f493cc2a8845f3e2d
Emmanuel Martin <em.martin456@gmail.com>
2025-08-21 10:16:06 +0200

    update

--- script.ts
+++ script.ts
@@ -1,20 +1,14 @@
-const form = document.getElementById("signup");
-const firstName = document.getElementById("firstName") as HTMLInputElement;
-const age = document.getElementById("age") as HTMLInputElement;
+// let
 
-const gift = (age: number) => {
-  age = age + 3
-  return age
-}
+// const
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  if (+age.value < 18) {
-    console.log("tu n'est pas majeur, tu ne peux pas voter!");
-  } else {
-    console.log(`Bonjour ${firstName.value} tu peux voter car tu as ${age.value} ans, tu aura droit à un cadeau quand tu auras ${gift(+age.value)} ans`);
-  }
+const pi = Math.PI;
+const API_KEY = "3434dESRE"
+console.log(pi);
 
 
-})
+// Let
+
+let number;
+number = 21
+console.log(number);
