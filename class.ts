97c47e0c7c3d5b6acdd5a8b7a2972b880c182422
Emmanuel Martin <em.martin456@gmail.com>
2025-08-28 11:53:11 +0200

    public, private, provited for class

--- class.ts
+++ class.ts
@@ -1,18 +1,65 @@
-// LES CLASSES
+ // LES CLASSES
 
-class Invoice {
-  client: string;
-  product: string;
-  price: number;
+// class Invoice {
+//   client: string;
+//   product: string;
+//   price: number;
 
-  constructor (client: string, product: string, price: number) {
-    this.client = client;
-    this.product = product;
-    this.price = price;
+//   constructor (client: string, product: string, price: number) {
+//     this.client = client;
+//     this.product = product;
+//     this.price = price;
+//   }
+
+// }
+
+// const invoice1 = new Invoice('Dora', 'TEmplate', 45);
+// const invoice2 = new Invoice("Nora","SEO", 200);
+// console.log(invoice1,invoice2);
+
+
+// Héritage
+
+class Mother {
+
+  constructor (public name: string, private color: string, protected eyes: number) {}
+
+  private speak () {
+    console.log(`Je m'appelle ${this.name} et j'ai les cheveux ${this.color}`);
+  }
+
+  //Getter
+
+  getName () {
+    return this.name
+  }
+
+  getColor () {
+    return this.color
+  }
+
+  // Setter
+
+  setColor (newColor: string) {
+    this.color = newColor
   }
 
 }
 
-const invoice1 = new Invoice('Dora', 'TEmplate', 45);
-const invoice2 : Invoice = {client:"Nora", product: "SEO", price: 200};
-console.log(invoice2);
+class Children extends Mother {
+  getData () {
+    /*
+      Public: propriété accessible partout et en dehors de la classe
+      Private: propriété accessible uniquement à l'intérieur de la classe Mother
+      Protected: propriété accessible uniquement dans la class Mère ou la class Enfant
+    */
+    return this.eyes
+  }
+
+};
+
+
+const person1 = new Mother ("Marie", "Chatain", 2)
+const person2 = new Children("Audrey", "Chatain", 2)
+person2.getData();
+console.log(person2.getData());
