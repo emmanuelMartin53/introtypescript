58fc2d66b2b0fb09a3a4ed3ef6650c5f9ff0b4e3
Emmanuel Martin <em.martin456@gmail.com>
2025-08-28 12:18:36 +0200

    update

--- class.js
+++ class.js
@@ -1,4 +1,19 @@
 // LES CLASSES
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Invoice = /** @class */ (function () {
     function Invoice(client, product, price) {
         this.client = client;
@@ -7,6 +22,48 @@ var Invoice = /** @class */ (function () {
     }
     return Invoice;
 }());
-var invoice1 = new Invoice('Dora', 'TEmplate', 45);
-var invoice2 = { client: "Nora", product: "SEO", price: 200 };
-console.log(invoice2);
+var invoice1 = new Invoice('Dora', 'Template', 45);
+var invoice2 = new Invoice('Nora', 'SEO', 200);
+var invoiceArray = [];
+invoiceArray.push(invoice1, invoice2);
+console.log(invoiceArray);
+// Héritage et Polymorphisme
+var Mother = /** @class */ (function () {
+    function Mother(name, color, eyes) {
+        this.name = name;
+        this.color = color;
+        this.eyes = eyes;
+    }
+    Mother.prototype.speak = function () {
+        console.log("je suis ".concat(this.name, " et j'ai les cheveux ").concat(this.color));
+    };
+    return Mother;
+}());
+var person1 = new Mother("marie", "Chatain", 2);
+// console.log(person1);
+// person1.speak();
+var Children = /** @class */ (function (_super) {
+    __extends(Children, _super);
+    function Children() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Children.prototype.speak = function () {
+        console.log("I am ".concat(this.name, " and i have  ").concat(this.color, " hair"));
+    };
+    Children.prototype.speakMotherLang = function () {
+        _super.prototype.speak.call(this);
+    };
+    return Children;
+}(Mother));
+var person2 = new Children("Déméter", "Brown", 2);
+// console.log(person2);
+// person2.speakMotherLang();
+var Random = /** @class */ (function () {
+    function Random(name, color, eyes) {
+        this.name = name;
+        this.color = color;
+        this.eyes = eyes;
+    }
+    return Random;
+}());
+var person3 = new Random("Tom", "Marron", 2);
--- class.ts
+++ class.ts
@@ -13,6 +13,61 @@ class Invoice {
 
 }
 
-const invoice1 = new Invoice('Dora', 'TEmplate', 45);
-const invoice2 : Invoice = {client:"Nora", product: "SEO", price: 200};
-console.log(invoice2);
+const invoice1 = new Invoice('Dora', 'Template', 45);
+const invoice2 = new Invoice('Nora', 'SEO', 200);
+
+let invoiceArray: Invoice[] = []
+invoiceArray.push(invoice1, invoice2)
+console.log(invoiceArray);
+
+
+// Héritage et Polymorphisme
+
+
+class Mother {
+  name: string;
+  color: string;
+  eyes: number;
+
+  constructor (name: string, color: string, eyes: number) {
+    this.name = name;
+    this.color = color;
+    this.eyes = eyes;
+  }
+
+  speak () {
+    console.log(`je suis ${this.name} et j'ai les cheveux ${this.color}`);
+
+  }
+}
+const person1 = new Mother("marie", "Chatain", 2);
+// console.log(person1);
+// person1.speak();
+
+
+class Children extends Mother {
+   speak () {
+    console.log(`I am ${this.name} and i have  ${this.color} hair`);
+  }
+  speakMotherLang() {
+    super.speak()
+  }
+}
+const person2: Mother = new Children("Déméter", "Brown", 2)
+// console.log(person2);
+// person2.speakMotherLang();
+
+
+class Random {
+    name: string;
+    color: string;
+    eyes: number;
+
+    constructor (name: string, color: string, eyes: number) {
+      this.name = name;
+      this.color = color;
+      this.eyes = eyes;
+    }
+}
+
+const person3: Mother = new Random("Tom", "Marron", 2)
